411683dd03d8dc70d078437d344be089ba58d4e9
faustfino <119843184+faustfino@users.noreply.github.com>
2023-11-07 21:23:57 +0100

    Slideshow

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,26 @@
-// CECILIE – Dropdown
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides fade");
+  let dots = document.getElementsByClassName("mySlides fade");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    slides[i].className = slides[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+
+}
